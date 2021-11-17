cf7cbf12c4fb98bb79d825f5ee6d9530bcf49695
nicolas jolys <jolys.nicolas@gmail.com>
2021-11-17 14:35:31 +0100

    rails new

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,9 @@
+class ReviewsController < ApplicationController
+
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+bishoom = { name: "Bishoom", address: "7 Boundary St, London", phone_number: "0299009900", category: "japanese" }
+dishoom = { name: "Dishoom", address: "8 Debby St, Paris", phone_number: "0299009901", category: "french" }
+eishoom = { name: "Eishoom", address: "9 Errie St, Toulouse", phone_number: "0299009902", category: "belgian" }
+fishoom = { name: "Fishoom", address: "10 Furie St, Lyon", phone_number: "0299009903", category: "french" }
+gishoom = { name: "Gishoom", address: "11 Gary St, London", phone_number: "0299009904", category: "japanese" }
+
+[ bishoom, dishoom, eishoom, fishoom, gishoom ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
